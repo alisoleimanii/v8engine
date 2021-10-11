aa3fc7391b3636dc3ea76d962a983efd147ac3c3
Reza Parsian <rezaparsian76@gmail.com>
2021-10-11 11:21:46 +0330

    booleanSelect

--- app/Helper/View/Filed/BooleanSelect.php
+++ app/Helper/View/Filed/BooleanSelect.php
@@ -6,12 +6,21 @@ namespace App\Helper\View\Filed;
 
 class BooleanSelect extends Select
 {
-    public function __construct()
+    public function __construct($attributes = [], $active = 1)
     {
-        return parent::__construct([
-            new Option(lang("base.active"), 1),
-            new Option(lang("base.di-active"), 0),
-        ]);
+        return parent::__construct(function () use ($active) {
+            if ($active)
+                return [
+                    new Option(lang("base.active"), 1),
+                    new Option(lang("base.di-active"), 0),
+                ];
+            return [
+                new Option(lang("base.di-active"), 0),
+                new Option(lang("base.active"), 1),
+            ];
+
+        }
+            , $attributes);
     }
 
 }
